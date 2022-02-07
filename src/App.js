c07d92ef35fa00e4c43a9048b546f734e961ca15
aditya <aditya_anand@condenast.com>
2022-02-07 14:10:58 +0530

    adding more charts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import cityRCA from "./citydata.json";
 import BarChartContainer from "./components/BarChart";
 import { Chart, registerables } from "chart.js";
+import DoughnutChartContainer from "./components/DoughnutChart";
+import { COLORS } from "./shared/constants";
 
 Chart.register(...registerables);
 
 function App() {
-  const data = {
+  const data = (keyName) => ({
     labels: cityRCA.map((c) => c.city),
     datasets: [
       {
         label: "Votes for cities",
-        data: cityRCA.map((c) => c["Total Votes"]),
-        backgroundColor: [
-          "rgb(255, 99, 132)",
-          "rgb(54, 162, 235)",
-          "rgb(255, 205, 86)",
-        ],
+        data: cityRCA.map((c) => c[keyName]),
+        backgroundColor: COLORS.slice(0, cityRCA.length),
         hoverOffset: 4,
       },
     ],
-  };
+  });
   return (
-    <div className="App">
-      <BarChartContainer
+    <div className="App charts-container">
+      <DoughnutChartContainer
         helpText="Number of votes per city"
-        dataSource={data}
+        dataSource={data("Total Votes")}
         header="Votes per city"
+      ></DoughnutChartContainer>
+
+      <BarChartContainer
+        helpText="Rating based on Total Overall %"
+        dataSource={data("TOTAL OVERALL %")}
+        header="Rating: Total Overall %"
+      ></BarChartContainer>
+      <BarChartContainer
+        helpText="Rating based on Total Venue Avg %"
+        dataSource={data("Total Venue Avg %")}
+        header="Rating: Total Venue Avg %"
+      ></BarChartContainer>
+      <BarChartContainer
+        helpText="Rating based on Total Feedback %"
+        dataSource={data("TOTAL TNS %")}
+        header="Rating: Total Feedback %"
       ></BarChartContainer>
     </div>
   );
