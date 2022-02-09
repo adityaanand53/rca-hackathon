9ae3ebaeca6435b34f861cc28bcb0410c5fef7c7
Sharique Asghar <sharique_asghar@condenast.com>
2022-02-09 20:07:01 +0530

    fixed cityRCA issue

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import cityRCA from "./../constants/citydata.json";
 import SideNav from "./SideNav";
 import BarChartContainer from "./BarChart";
 import DoughnutChartContainer from "./DoughnutChart";
