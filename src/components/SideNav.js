1f0a83eafd437978e8f4c18e62ff769b0e131ca8
aditya <aditya_anand@condenast.com>
2022-02-24 15:05:29 +0530

    side nav update

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -4,11 +4,36 @@ import { Link } from "react-router-dom";
 const SideNav = () => {
   return (
     <nav className="sidenav">
+      <h1 className="header">RCA Data Visualization</h1>
       <Link className="navlinks" to="/city">
         City
       </Link>
       <Link className="navlinks" to="/islands">
         Islands
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Country
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Your Year In Travel
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Airport
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Airlines
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Beaches
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Destination Spas
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Ski Resorts
+      </Link>
+      <Link className="navlinks" to="/islands">
+        Luggage
       </Link>
     </nav>
   );
